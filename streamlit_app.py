fa7d08c97d3eb1c2cad9ec895a420f499edfd4f6
Brian-J-Prather <85587416+Brian-J-Prather@users.noreply.github.com>
2022-07-22 17:13:36 -0500

    Update streamlit_app.py
    Removed streamlit Stop function and corrected syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
 #New Section to allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
@@ -69,5 +67,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function
+  streamlit.text(back_from_function)
 
